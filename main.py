17c4b80fdf11d966f0233245c3d3d269ed73e24d
Miguel Calderon <miguelcalderon.dev@gmail.com>
2024-10-08 09:42:47 -0500

    api finalizada

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ app.add_middleware(
 
 
 def obtenerClasificacion(path):
-    pipe = pipeline("image-classification", model="MiguelCalderon/google-vit-base-patch16-224-OrganicAndInorganicWaste")
+    pipe = pipeline("image-classification", model="MiguelCalderon/google-vit-base-patch16-224-Waste-O-I-classification")
     prediction = pipe(path)
     class_label = prediction[0]['label']
     probability = prediction[0]['score']
     return class_label, probability
 
-@app.get("/hola-mundo")
-async def hola_mundo():
-    return {"mensaje": "Hola Mundo"}
+@app.get("/getDataPrueba")
+def prueba():
+    return {"label": "O", "score": "https://t1.uc.ltmcdn.com/es/posts/8/8/1/que_se_tira_en_el_contenedor_organico_33188_600.jpg"}
 
 @app.post("/clasificarImagen")
 async def clasificarImagen(image: UploadFile = File(...)):
@@ -38,14 +38,37 @@ async def clasificarImagen(image: UploadFile = File(...)):
             shutil.copyfileobj(image.file, temp_image)
             temp_image_path = temp_image.name
 
-        class_label, _ = obtenerClasificacion(temp_image_path)  # Obtener la clasificación
+        class_label, score = obtenerClasificacion(temp_image_path)  # Obtener la clasificación
         #base_path = r"C:\apitesis\clasification"
         #class_folder_path = os.path.join(base_path, class_label) # Crear la carpeta para la clasificación si no existe
         #os.makedirs(class_folder_path, exist_ok=True)
         #new_image_path = os.path.join(class_folder_path, image.filename)# Mover la imagen al directorio correspondiente
         #shutil.move(temp_image_path, new_image_path)
 
-        return {"label": class_label}
+        return {"label": class_label, "score": score}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/clasificarYguadarImagen")
+async def clasificarYguardarImagen(image: UploadFile = File(...)):
+    if not (image.filename.endswith(".svg") or image.filename.endswith(".png") or image.filename.endswith(
+            ".jpeg") or image.filename.endswith(".jpg")):
+        raise HTTPException(status_code=400, detail="El archivo debe ser una imagen.")
+    try:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_image:
+            shutil.copyfileobj(image.file, temp_image)
+            temp_image_path = temp_image.name
+
+        class_label, score = obtenerClasificacion(temp_image_path)
+        base_path = r"C:\apitesis\clasification"
+        class_folder_path = os.path.join(base_path, class_label)
+        os.makedirs(class_folder_path, exist_ok=True)
+        new_image_path = os.path.join(class_folder_path, image.filename)
+
+        shutil.move(temp_image_path, new_image_path)
+
+        return {"label": class_label, "score": score}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
